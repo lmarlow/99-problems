b6ff4629db5dd87a8aca0a62fc61f4d169a1880f
Lee Marlow <lee.marlow@gmail.com>
2010-03-03 00:21:17 -0700

    Use R for the result variable

--- prolog/ninety-nine.pl
+++ prolog/ninety-nine.pl
@@ -3,28 +3,28 @@
 
 % P01 (*) Find the last element of a list.
 % Example:
-% ?- my_last(X,[a,b,c,d]).
-% X = d
-my_last(X,[X]).
-my_last(X, [_|T]) :- my_last(X, T).
+% ?- my_last(R,[a,b,c,d]).
+% R = d
+my_last(R,[R]).
+my_last(R, [_|T]) :- my_last(R, T).
 
 % P02 (*) Find the last but one element of a list.
-my_last_but_one(X, [X, _]).
-my_last_but_one(X, [_|T]) :- my_last_but_one(X, T).
+my_last_but_one(R, [R, _]).
+my_last_but_one(R, [_|T]) :- my_last_but_one(R, T).
 
 % P03 (*) Find the K'th element of a list.
 % The first element in the list is number 1.
 % Example:
-% ?- element_at(X,[a,b,c,d,e],3).
-% X = c
-element_at(X, [X|_], 1).
-element_at(X, [_|T], K) :- K > 1, J is K - 1, element_at(X, T, J).
+% ?- element_at(R,[a,b,c,d,e],3).
+% R = c
+element_at(R, [R|_], 1).
+element_at(R, [_|T], K) :- K > 1, J is K - 1, element_at(R, T, J).
 
 % P04 (*) Find the number of elements of a list.
 number_of_elements(0, []).
-number_of_elements(X, [_|T]) :- number_of_elements(Y, T), X is Y + 1.
+number_of_elements(R, [_|T]) :- number_of_elements(Y, T), R is Y + 1.
 
 % P05 (*) Reverse a list.
-revAcc([], X, X).
-revAcc([H|T],Acc, Y) :- NewAcc = [H|Acc], revAcc(T, NewAcc, Y).
-my_reverse(X, Y) :- revAcc(X, [], Y).
+revAcc([], R, R).
+revAcc([H|T],Acc, R) :- revAcc(T, [H|Acc], R).
+my_reverse(X, R) :- revAcc(X, [], R).
